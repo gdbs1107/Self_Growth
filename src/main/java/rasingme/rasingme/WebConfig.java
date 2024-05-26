0e46385bf72f731f5235dee0f6da9403b7538427
sunrox <gdbs1107@naver.com>
2024-05-26 19:16:34 +0900

    FK and Username Fix

--- src/main/java/rasingme/rasingme/WebConfig.java
+++ src/main/java/rasingme/rasingme/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://port-0-rasingme-1ru12mlwbsd5mh.sel5.cloudtype.app/") // 허용할 출처
+                .allowedOrigins("https://port-0-rasingme-1ru12mlwbsd5mh.sel5.cloudtype.app/",
+                        "http://localhost:5173") // 허용할 출처
                 .allowedMethods("GET", "POST","PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000); // 원하는 시간만큼 pre-flight 리퀘스트를 캐싱
